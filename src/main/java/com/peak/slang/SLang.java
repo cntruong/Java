e960b3d769eb16598032dcfb0d5ba26d9aee91e9
cntruong <46119084+cntruong@users.noreply.github.com>
2023-06-20 22:36:48 +0700

    Code giao diện

--- src/main/java/com/peak/slang/SLang.java
+++ src/main/java/com/peak/slang/SLang.java
@@ -21,28 +21,7 @@ import java.util.HashMap;
  * @author TRUONG
  */
 public class SLang  {
-//    public SLang()
-//    {
-//        setLayout(new BorderLayout());
-//        JButton btnSearch =new JButton("Tìm");
-//        btnSearch.setPreferredSize(new Dimension(120,25));
-//        add(btnSearch,BorderLayout.CENTER);
-//        JPanel pTop =new JPanel();
-//        pTop.setLayout(new BoxLayout(pTop,BoxLayout.LINE_AXIS));
-//        JButton btnTest =new JButton("Test");
-//        pTop.add(btnTest);
-//        add(pTop,BorderLayout.PAGE_START);
-//    }
-//    private static void createAndShowGUI(){
-//        JFrame.setDefaultLookAndFeelDecorated(true);
-//        JFrame frm=new JFrame("Tra từ điển");
-//        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        SLang newContentPane =new SLang();
-//        newContentPane.setOpaque(true);
-//        frm.setContentPane(newContentPane);
-//        frm.pack();
-//        frm.setVisible(true);
-//    }
+
     public static void main(String[] args) {
         // TODO code application logic here
         CardLayout cl = new CardLayout();
